9b4b9dcb85a37a70b569ed5f97c9061a34da426d
stewe12 <stepan.kriz@gmail.com>
2024-06-26 12:46:10 +0200

    Update config_flow.py

--- custom_components/stk_czechr/config_flow.py
+++ custom_components/stk_czechr/config_flow.py
@@ -1,15 +1,6 @@
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.core import callback
 from .const import DOMAIN, CONF_NAME, CONF_VIN
-import logging
-
-_LOGGER = logging.getLogger(__name__)
-
-@callback
-def configured_cars(hass):
-    """Return a set of configured VINs."""
-    return set(entry.data[CONF_VIN] for entry in hass.config_entries.async_entries(DOMAIN))
 
 class STKczechrConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for STK czechr."""
@@ -17,24 +8,20 @@ class STKczechrConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
 
-    def __init__(self):
-        self.logger = _LOGGER  # Assign the logger in the constructor
-
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
         errors = {}
         if user_input is not None:
-            self.logger.debug("User input received: %s", user_input)
-            if user_input[CONF_VIN] in configured_cars(self.hass):
+            existing_entries = [entry.data[CONF_VIN] for entry in self._async_current_entries()]
+            if user_input[CONF_VIN] in existing_entries:
                 errors["base"] = "vin_exists"
             else:
-                self.logger.debug("Creating entry with data: %s", user_input)
                 return self.async_create_entry(title=user_input[CONF_NAME], data=user_input)
 
         data_schema = vol.Schema(
             {
-                vol.Required("name", description={"suggested_value": "Jméno vozidla"}): str,
-                vol.Required("vin", description={"suggested_value": "VIN"}): str,
+                vol.Required(CONF_NAME, description={"suggested_value": "Jméno vozidla"}): str,
+                vol.Required(CONF_VIN, description={"suggested_value": "VIN"}): str,
             }
         )
         return self.async_show_form(step_id="user", data_schema=data_schema, errors=errors)
