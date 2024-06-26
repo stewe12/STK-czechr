d38dd6f5b371c24e734250a283d3cc0929889469
stewe12 <stepan.kriz@gmail.com>
2024-06-26 09:33:21 +0200

    Update config_flow.py

--- custom_components/stk_czechr/config_flow.py
+++ custom_components/stk_czechr/config_flow.py
@@ -12,7 +12,6 @@ class STKczechrConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for STK czechr."""
 
     VERSION = 1
-    CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
