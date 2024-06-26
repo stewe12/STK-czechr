cadf6b6d5b37d7c2632a047ae6a9e136e41d4f6c
stewe12 <stepan.kriz@gmail.com>
2024-06-26 10:00:07 +0200

    Update config_flow.py

--- custom_components/stk_czechr/config_flow.py
+++ custom_components/stk_czechr/config_flow.py
@@ -1,8 +1,7 @@
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.core import callback
-from homeassistant.helpers import config_validation as cv
-from .const import DOMAIN
+from .const import DOMAIN, CONF_NAME, CONF_VIN
 
 @callback
 def configured_cars(hass):
@@ -26,8 +25,8 @@ class STKczechrConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         data_schema = vol.Schema(
             {
-                vol.Required("name"): str,
-                vol.Required("vin"): str,
+                vol.Required(CONF_NAME): str,
+                vol.Required(CONF_VIN): str,
             }
         )
         return self.async_show_form(step_id="user", data_schema=data_schema, errors=errors)
