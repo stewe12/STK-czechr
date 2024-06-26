c44413694ded77d206d192dbb00feada4d6658dd
stewe12 <stepan.kriz@gmail.com>
2024-06-26 08:25:44 +0200

    Update run.sh

--- stk_czechr/run.sh
+++ stk_czechr/run.sh
@@ -1,9 +1,3 @@
-#!/bin/bash
-
-set -e
-
-# Set up environment for Python script
-export PYTHONPATH=/usr/lib/python3.9/site-packages
-
-# Start the Python script
-python3 /fetch_car_inspection.py
+#!/usr/bin/with-contenv bashio
+echo "Starting STK czechr add-on..."
+python3 /app/fetch_car_inspection.py
