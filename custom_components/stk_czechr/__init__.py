222f7c454d94d6e194a207adf5adb506979045d5
stewe12 <stepan.kriz@gmail.com>
2024-06-26 09:52:41 +0200

    Update __init__.py

--- custom_components/stk_czechr/__init__.py
+++ custom_components/stk_czechr/__init__.py
@@ -1,5 +1,6 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+
 from .const import DOMAIN
 
 async def async_setup(hass: HomeAssistant, config: dict):
