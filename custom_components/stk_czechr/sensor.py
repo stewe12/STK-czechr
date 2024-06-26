f3cb847931449e8fb8b3f050a70d6ce7deba2737
stewe12 <stepan.kriz@gmail.com>
2024-06-26 10:21:47 +0200

    Update sensor.py

--- custom_components/stk_czechr/sensor.py
+++ custom_components/stk_czechr/sensor.py
@@ -1,12 +1,17 @@
 import aiohttp
 import async_timeout
+import logging
 from homeassistant.components.sensor import SensorEntity
-from .const import CONF_NAME, CONF_VIN
+from homeassistant.const import CONF_NAME, CONF_VIN
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass, entry, async_add_entities):
     """Set up STK czechr sensors from a config entry."""
     name = entry.data[CONF_NAME]
     vin = entry.data[CONF_VIN]
+    _LOGGER.debug("Setting up sensor with name: %s, vin: %s", name, vin)
     async_add_entities([STKczechrSensor(name, vin)], True)
 
 class STKczechrSensor(SensorEntity):
@@ -37,14 +42,17 @@ class STKczechrSensor(SensorEntity):
     async def async_update(self):
         """Fetch new state data for the sensor."""
         url = f"https://www.dataovozidlech.cz/api/Vozidlo/GetVehicleInfo?vin={self._vin}"
+        _LOGGER.debug("Fetching data from URL: %s", url)
         try:
             async with aiohttp.ClientSession() as session:
                 with async_timeout.timeout(10):
                     response = await session.get(url)
                     data = await response.json()
+                    _LOGGER.debug("Received data: %s", data)
                     self._state = data[0]["value"]  # Assuming the first element represents the state
                     self._attributes = {item["label"]: item["value"] for item in data}
         except Exception as e:
+            _LOGGER.error("Error fetching data: %s", e)
             self._state = None
             self._attributes = {}
             self._attributes["error"] = str(e)
