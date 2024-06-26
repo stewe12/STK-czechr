e1d15bdaa7af56a38a9e7ce441e7906429e734c0
stewe12 <stepan.kriz@gmail.com>
2024-06-26 11:17:27 +0200

    Update sensor.py

--- custom_components/stk_czechr/sensor.py
+++ custom_components/stk_czechr/sensor.py
@@ -1,7 +1,10 @@
+import asyncio
 import aiohttp
 import async_timeout
+from datetime import timedelta
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.const import CONF_NAME, CONF_VIN
+from homeassistant.helpers.update_coordinator import CoordinatorEntity, DataUpdateCoordinator
 
 from .const import DOMAIN
 
@@ -9,36 +12,28 @@ async def async_setup_entry(hass, entry, async_add_entities):
     """Set up STK czechr sensors from a config entry."""
     name = entry.data[CONF_NAME]
     vin = entry.data[CONF_VIN]
-    async_add_entities([STKczechrSensor(name, vin)], True)
+    coordinator = STKczechrDataUpdateCoordinator(hass, name, vin)
+    async_add_entities([STKczechrSensor(coordinator)], True)
 
-class STKczechrSensor(SensorEntity):
-    """Representation of a STK czechr sensor."""
+class STKczechrDataUpdateCoordinator(DataUpdateCoordinator):
+    """Class to manage fetching data from the API."""
 
-    def __init__(self, name, vin):
-        """Initialize the sensor."""
-        self._name = name
-        self._vin = vin
+    def __init__(self, hass, name, vin):
+        self.hass = hass
+        self.name = name
+        self.vin = vin
         self._state = None
         self._attributes = {}
+        super().__init__(
+            hass,
+            asyncio.get_event_loop(),
+            name=f"{name} Data",
+            update_interval=timedelta(days=1),  # Update daily
+        )
 
-    @property
-    def name(self):
-        """Return the name of the sensor."""
-        return self._name
-
-    @property
-    def state(self):
-        """Return the state of the sensor."""
-        return self._state
-
-    @property
-    def extra_state_attributes(self):
-        """Return the state attributes."""
-        return self._attributes
-
-    async def async_update(self):
-        """Fetch new state data for the sensor."""
-        url = f"https://www.dataovozidlech.cz/api/Vozidlo/GetVehicleInfo?vin={self._vin}"
+    async def _async_update_data(self):
+        """Fetch data from the API."""
+        url = f"https://www.dataovozidlech.cz/api/Vozidlo/GetVehicleInfo?vin={self.vin}"
         try:
             async with aiohttp.ClientSession() as session:
                 with async_timeout.timeout(10):
@@ -50,3 +45,31 @@ class STKczechrSensor(SensorEntity):
             self._state = None
             self._attributes = {}
             self._attributes["error"] = str(e)
+        
+        return self._state, self._attributes
+
+class STKczechrSensor(SensorEntity):
+    """Representation of a STK czechr sensor."""
+
+    def __init__(self, coordinator):
+        """Initialize the sensor."""
+        self._coordinator = coordinator
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._coordinator.name
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._coordinator.data[0] if self._coordinator.data else None
+
+    @property
+    def extra_state_attributes(self):
+        """Return the state attributes."""
+        return self._coordinator.data[1] if self._coordinator.data else {}
+
+    async def async_update(self):
+        """Fetch new state data for the sensor."""
+        await self._coordinator.async_request_refresh()
