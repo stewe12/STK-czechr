9fb7ee0705831aedaade5c9ac3a118d6bf91d189
stewe12 <stepan.kriz@gmail.com>
2024-11-17 15:34:16 +0100

    Update __init__.py

--- custom_components/stk_czechr/__init__.py
+++ custom_components/stk_czechr/__init__.py
@@ -1,36 +1,30 @@
+"""The STK czechr integration."""
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from .const import DOMAIN
+from .const import DOMAIN, PLATFORM_SENSOR
 
 import logging
 
 _LOGGER = logging.getLogger(__name__)
 
+PLATFORMS = [PLATFORM_SENSOR]
+
 async def async_setup(hass: HomeAssistant, config: dict):
     """Set up the STK czechr component."""
     hass.data.setdefault(DOMAIN, {})
-    _LOGGER.info("STK czechr integration setup completed.")
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up STK czechr from a config entry."""
-    if DOMAIN not in hass.data:
-        hass.data[DOMAIN] = {}
-
-    # Uložení konfigurace konkrétního vstupu
+    hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
 
-    _LOGGER.info("Setting up STK czechr for entry: %s", entry.entry_id)
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload a config entry."""
-    if entry.entry_id in hass.data.get(DOMAIN, {}):
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
-        _LOGGER.info("Unloading STK czechr for entry: %s", entry.entry_id)
-
-    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
-    return True
+    return unload_ok
